eb71ff7b3d5ea2d0b98821528b74c5e88699296a
kost90 <kostdonc90@gmail.com>
2023-09-29 12:33:42 +0100

    added .env file

--- src/components/contactform/ContactFrom.js
+++ src/components/contactform/ContactFrom.js
@@ -6,6 +6,8 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { flex_container_form } from "./ContactForm.module.css";
 import emailjs from '@emailjs/browser';
 
+const key = process.env.REACT_APP_PUBLIC_KEY;
+
 const phoneNumberRegex = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
 
 const contactSchema = {
@@ -57,7 +59,7 @@ const ContactForm = memo(() => {
   });
 
   const onSubmit = useCallback(() => {
-    emailjs.sendForm('portfolio', 'contact_form', form.current, 'd6kWRCJ_PncEG8pQY')
+    emailjs.sendForm('portfolio', 'contact_form', form.current, key)
     console.log('email send');
     reset();
   }, []);
